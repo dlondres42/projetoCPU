8bc288d43204bb3d9a44979cbf15d02635babce2
Yves <yefo@cin.ufpe.br>
2021-11-23 19:06:49 -0300

    finishing CPU

--- project components/made_components/MuxMem.v
+++ project components/made_components/MuxMem.v
@@ -1,11 +1,11 @@
 module MuxMem(
     input wire selector,
     input wire [31:0] data0,
-    input wire [31:0] data1,
+    input wire [15:0] data1,
 
     output wire [31:0] data_out
 );
 
-    assign data_out = (selector == 2'b00) ? data0 : data1;
+    assign data_out = (selector == 2'b00) ? data0 : {{16{1'b0}}, data1};
 
 endmodule
